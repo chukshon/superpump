e81e21a2eb2794abbd22764ba938835c1146e729
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-05-28 12:39:54 +0100

    minor edits to the connect wallet component

--- components/header/ConnectWalletHeader.tsx
+++ components/header/ConnectWalletHeader.tsx
@@ -3,27 +3,36 @@ import React from "react";
 import { MdOutlineLogout } from "react-icons/md";
 
 const ConnectWalletHeader = () => {
-  const isConnected = false;
+  const isConnected = true;
 
   return (
     <div>
       {isConnected ? (
-        <div className="flex items-center gap-[15px] cursor-pointer z-30">
-          <Image
-            src="/icons/eth-logo.svg"
-            width="32"
-            height="32"
-            alt="Eth Logo"
-            className="z-30"
-          />
+        <div>
+          <button
+            type="button"
+            className="lg:hidden rounded-[4px] bg-custom-radial py-[10px] px-[30px]"
+          >
+            Dashboard
+          </button>
 
-          <p className="text-[#B2B2B2] z-30">0X57db....A17B</p>
-          <Image
-            src="/icons/logout.svg"
-            width="18"
-            height="19"
-            alt="Eth Logo"
-          />
+          <div className="hidden lg:flex items-center gap-[15px] cursor-pointer z-30">
+            <Image
+              src="/icons/eth-logo.svg"
+              width="32"
+              height="32"
+              alt="Eth Logo"
+              className="z-30"
+            />
+
+            <p className="text-[#B2B2B2] z-30">0X57db....A17B</p>
+            <Image
+              src="/icons/logout.svg"
+              width="18"
+              height="19"
+              alt="Eth Logo"
+            />
+          </div>
         </div>
       ) : (
         <button
