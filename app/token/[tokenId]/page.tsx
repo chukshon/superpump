77ce0c98c43ff2d879e0ff3324bdf3b39538a9cf
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-06-06 10:28:27 +0100

    started working on the token action section in the left col

--- app/token/[tokenId]/page.tsx
+++ app/token/[tokenId]/page.tsx
@@ -1,5 +1,8 @@
+import ChatSection from "@/containers/home/ChatSection";
 import ChartView from "@/containers/tokenDetails/ChartView";
 import TopBar from "@/containers/tokenDetails/TopBar";
+import TradeDetailsBox from "@/containers/tokenDetails/TradeDetailsBox";
+import TradeItem from "@/containers/tokenDetails/TradeItem";
 import React from "react";
 
 const TokenDetails = () => {
@@ -9,13 +12,28 @@ const TokenDetails = () => {
       <TopBar />
 
       <div className=" flex items-start mt-[30px] gap-[20px] container">
-        {/* Left Col */}
-        <div className="w-[70%] h-[1000px] border-[1px] border-neutral-0">
+        {/* Chart Left Col */}
+        <div className="w-[70%] border-[1px] border-neutral-0">
           <ChartView />
         </div>
 
-        {/* Right Col */}
-        <div className="w-[30%]  h-[1000px] bg-blue-400">Right</div>
+        {/* Token Actions Right Col */}
+        <div className="w-[30%]  border-[1px] border-neutral-0 px-[10px] py-[10px] rounded-[8px]">
+          {/* Trade details */}
+          <div className="mt-[10px]">
+            <TradeDetailsBox />
+          </div>
+
+          {/* Buy and sell Section */}
+          <div className="my-[10px]">
+            <TradeItem />
+          </div>
+
+          {/* Chat Section */}
+          <div>
+            <ChatSection />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ containers/tokenDetails/TradeItem.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import PriceItem from "./PriceItem";
+import Image from "next/image";
+
+const TradeItem = () => {
+  return (
+    <div>
+      {/* Buy and sell Tab */}
+      <ul className="flex">
+        <li className="flex items-center justify-center w-[50%] py-[10px] bg-red-500 rounded-tl-[5px] rounded-tr-[5px]">
+          Buy
+        </li>
+        <li className="flex items-center justify-center w-[50%] py-[10px] bg-blue-500 rounded-tl-[5px] rounded-tr-[5px]">
+          Sell
+        </li>
+      </ul>
+
+      <div className="border-[1px] border-neutral-0 rounded-bl-[5px] rounded-br-[5px] py-[20px] px-[30px]">
+        {/* Fixed Price List */}
+        <ul>
+          <PriceItem />
+        </ul>
+
+        {/* Price Input */}
+        <div className="mt-[10px]">
+          <div className="border-[1px] border-neutral-150 flex gap-[4px] items-center pl-[10px] py-[10px] rounded-[10px]">
+            <Image
+              src="/icons/ftm-logo-priceitem.svg"
+              width={20}
+              height={20}
+              alt="ftm logo superpump fun trade"
+              className="w-[14px] h-[16px]"
+            />
+            <input
+              placeholder="Enter amount in FTM"
+              className="w-[70%] outline-none bg-[transparent] placeholder:text-[16px] text-[16px] placeholder:text-[#4A4A4ACC] text-neutral-50 "
+            />
+          </div>
+        </div>
+
+        {/* Advanced Settings */}
+        <button
+          type="button"
+          className="text-[#4A4A4A] font-chakra_Petch text-[13px] w-full py-[10px]"
+        >
+          <p className="text-right">Advanced Settings</p>
+        </button>
+
+        {/* Quick Buy/Sell */}
+        <button
+          type="button"
+          className="bg-green-600 w-full text-center py-[10px]"
+        >
+          Quick Buy
+        </button>
+
+        <p className="text-[#4A4A4A] text-[12px] mt-[10px]">
+          By clicking “Quick Buy”, you agree to have your transaction
+          immediately sent.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TradeItem;
