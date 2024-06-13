f39441f41fd674d1c57f4580dc5e60444beafd7a
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-06-13 20:46:14 +0100

    renamed leaderboard to marketUpdate

---
+++ containers/home/MarketUpdateSection/MarketUpdateItem.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UnionBorderContainer from "@/components/ui/UnionBorderContainer";
+import { DefaultTable } from "@/components/table/DefaultTable/DefaultTable";
+import Image from "next/image";
+
+const MarketUpdateItem = () => {
+  return (
+    <UnionBorderContainer>
+      <DefaultTable />
+    </UnionBorderContainer>
+  );
+};
+
+export default MarketUpdateItem;
