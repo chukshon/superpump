9304c46645c88e75ef5408aac7a27661a0d5c2dd
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-06-15 10:41:21 +0100

    wrapped up the market update section

--- containers/home/components/MarketUpdateSection/useMarketUpdate.tsx
+++ containers/home/components/MarketUpdateSection/useMarketUpdate.tsx
@@ -1,14 +1,5 @@
 import React from "react";
-import {
-  ColumnDef,
-  SortingState,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 import { HiChevronUpDown } from "react-icons/hi2";
 
 const MarketUpdateData: MarketUpdateT[] = [
