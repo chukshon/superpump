8a067defc32b347b2e45b603d2e73d689a84c4df
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-06-27 07:04:29 +0100

    Made the presale top bar responsive

--- app/token/[tokenId]/presale/page.tsx
+++ app/token/[tokenId]/presale/page.tsx
@@ -3,9 +3,22 @@ import PresaleFilterTopBar from "@/containers/presale/PresaleFilterTopBar";
 import React from "react";
 
 const Presale = () => {
+  const PRESALE_LIST_DATA = [
+    {
+      id: "1",
+      name: "Pope Pepe",
+      status: "Live Sale",
+      image: "",
+      type: "Presale",
+      softCap: 1000,
+      liquidity: 90,
+      lockupTime: "180 days",
+      salesEnd: "01:17:33:01",
+    },
+  ];
   return (
-    <main className="border-[1px] border-b-[0px] mx-[20px] my-[10px] border-neutral-0 h-[100vh]">
-      <div className="py-[50px] px-[50px]">
+    <main className="lg:border-[1px] border-b-[0px] mx-[20px] my-[10px] border-neutral-0 h-[100vh]">
+      <div className="lg:py-[50px] lg:px-[50px] py-[10px]">
         {/* Top Bar */}
         <PresaleFilterTopBar />
 
--- containers/presale/PresaleFilterTopBar.tsx
+++ containers/presale/PresaleFilterTopBar.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 const PresaleFilterTopBar = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-[20px]">
       {/* Input */}
       <input
         placeholder="Type token symbol or address to find your Sale"
-        className="py-[10px] px-[10px] w-[50%] outline-none bg-[transparent] placeholder:text-[16px] text-[16px] placeholder:text-[#4A4A4ACC] text-neutral-50 active:bg-[transparent] placeholder:font-[600] font-space_grotesk border-[1px]"
+        className="py-[10px] px-[10px] lg:w-[50%] outline-none bg-[transparent] placeholder:text-[16px] text-[16px] placeholder:text-[#4A4A4ACC] text-neutral-50 active:bg-[transparent] placeholder:font-[600] font-space_grotesk border-[1px]"
       />
       {/* Filters container */}
-      <div className="flex gap-[10px]">
-        <SelectVariantOne className="min-w-[250px]" />
-        <SelectVariantOne className="min-w-[250px]" />
+      <div className="flex gap-[10px] flex-col lg:flex-row">
+        <SelectVariantOne className="lg:min-w-[250px]" />
+        <SelectVariantOne className="lg:min-w-[250px]" />
       </div>
     </div>
   );
