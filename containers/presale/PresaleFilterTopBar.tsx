81ea733b55973fcd9ab9460ee04c9bede30e8d30
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-06-28 11:02:01 +0100

    minor edit

--- containers/presale/PresaleFilterTopBar.tsx
+++ containers/presale/PresaleFilterTopBar.tsx
@@ -7,7 +7,7 @@ const PresaleFilterTopBar = () => {
       {/* Input */}
       <input
         placeholder="Type token symbol or address to find your Sale"
-        className="py-[10px] px-[10px] lg:w-[50%] outline-none bg-[transparent] placeholder:text-[16px] text-[16px] placeholder:text-[#4A4A4ACC] text-neutral-50 active:bg-[transparent] placeholder:font-[600] font-space_grotesk border-[1px]"
+        className="z-40 py-[10px] px-[10px] lg:w-[50%] outline-none bg-[transparent] placeholder:text-[16px] text-[16px] placeholder:text-[#4A4A4ACC] text-neutral-50 active:bg-[transparent] placeholder:font-[600] font-space_grotesk border-[1px]"
       />
       {/* Filters container */}
       <div className="flex gap-[10px] flex-col lg:flex-row">
