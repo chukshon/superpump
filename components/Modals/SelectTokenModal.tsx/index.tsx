706ac5ab70332ad77cb613f268a42dfdb3989a1f
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-05 09:27:46 +0100

    styled the input in the select token modal

--- components/Modals/SelectTokenModal.tsx/index.tsx
+++ components/Modals/SelectTokenModal.tsx/index.tsx
@@ -4,16 +4,21 @@ import SuggestedTokenItem from "./SuggestedTokenItem";
 
 const SelectTokenModal = () => {
   return (
-    <Modal modalHeaderTitle={`Select a token]`} width={514}>
-      <input
-        placeholder="Search name or Address"
-        className="border-[#306392] border-[1.5px] rounded-[4px] placeholder:text-[#595959] placeholder:text-[12px]"
-      />
-      {/* Suggested Token List */}
-      <ul className="grid grid-cols-3 gap-x-[10px] gap-y-[10px]">
-        <SuggestedTokenItem />
-        <SuggestedTokenItem />
-      </ul>
+    <Modal modalHeaderTitle={`Select a token`} width={400}>
+      <div className="px-[10px]">
+        <input
+          placeholder="Search name or Address"
+          className="w-full input-linear-gradient  border-[#306392] border-[1px] rounded-[4px] placeholder:text-[#595959] placeholder:text-[15px] py-[5px] placeholder:text-center outline-none text-neutral-0"
+        />
+        {/* Suggested Token List */}
+        <ul className="grid grid-cols-3 gap-x-[10px] gap-y-[10px] py-[20px]">
+          <SuggestedTokenItem />
+          <SuggestedTokenItem />
+          <SuggestedTokenItem />
+          <SuggestedTokenItem />
+          <SuggestedTokenItem />
+        </ul>
+      </div>
     </Modal>
   );
 };
