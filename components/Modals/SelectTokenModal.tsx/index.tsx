bc33fd70287cc4dd64d0ad7a00797cd75cc02c3d
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-05 09:09:03 +0100

    created the select token modal

---
+++ components/Modals/SelectTokenModal.tsx/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Modal from "../Modal";
+
+const SelectTokenModal = () => {
+  return (
+    <Modal modalHeaderTitle={`Report [“User” or “Content”]`} width={514}>
+      <div className="h-[900px] ">Hey</div>
+    </Modal>
+  );
+};
+
+export default SelectTokenModal;
