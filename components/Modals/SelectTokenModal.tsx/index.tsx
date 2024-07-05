2262605b7ee0d753f8d2ef59b8a4ed57742bfa06
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-05 09:15:39 +0100

    Added the search input component to the select token modal

--- components/Modals/SelectTokenModal.tsx/index.tsx
+++ components/Modals/SelectTokenModal.tsx/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Modal from "../Modal";
+import SuggestedTokenItem from "./SuggestedTokenItem";
 
 const SelectTokenModal = () => {
   return (
     <Modal modalHeaderTitle={`Select a token]`} width={514}>
-      <div className="h-[900px] ">Hey</div>
+      <input
+        placeholder="Search name or Address"
+        className="border-[#306392] border-[1.5px] rounded-[4px] placeholder:text-[#595959] placeholder:text-[12px]"
+      />
     </Modal>
   );
 };
