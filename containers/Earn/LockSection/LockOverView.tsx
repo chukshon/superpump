1fc4e48cb4cb8054c4b1e0a7b9ba439de0398c01
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-06 06:46:16 +0100

    created the lock overview and stake section component

---
+++ containers/Earn/LockSection/LockOverView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LockOverView = () => {
+  return <div>Lock Overview</div>;
+};
+
+export default LockOverView;
--- containers/Earn/LockSection/LockSection.tsx
+++ containers/Earn/LockSection/LockSection.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import LockOverView from "./LockOverView";
+import StakeSection from "./StakeSection";
 
 const LockSection = () => {
-  return <div>Lock Section</div>;
+  return (
+    <div className="border-[#172532] rounded-[8px] px-[20px] lg:px-[30px]">
+      <div></div>
+      Lock Section
+    </div>
+  );
 };
 
 export default LockSection;
