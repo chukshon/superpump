be17a17def3cd2b6106936c13a0f501ddf7295df
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-06 07:30:13 +0100

    Added the input tabs to the duration selector section

--- containers/Earn/LockSection/StakeSection/DurationSelectorSection.tsx
+++ containers/Earn/LockSection/StakeSection/DurationSelectorSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DurationSelector from "../../components/DurationSelector";
+import InputTab from "../../components/InputTab";
 
 const DurationSelectorSection = () => {
   return (
@@ -8,6 +9,12 @@ const DurationSelectorSection = () => {
         Add token
       </h3>
       <DurationSelector />
+      <ul className="flex justify-between w-full mt-[20px]">
+        <InputTab value="1W" />
+        <InputTab value="1M" />
+        <InputTab value="3M" />
+        <InputTab value="6M" />
+      </ul>
     </div>
   );
 };
--- containers/Earn/components/InputTab.tsx
+++ containers/Earn/components/InputTab.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const InputTab = ({ value }: { value: string }) => {
   return (
-    <button className="rounded-[4px] text-neutral-0 border-[1px] border-[#112545] bg-[rgba(0,0,0,0.24)] px-[20px] py-[1px]">
+    <button className="text-[13px] rounded-[4px] text-neutral-0 border-[1px] border-[#112545] bg-[rgba(0,0,0,0.24)] px-[20px] py-[1px]">
       {value}
     </button>
   );
