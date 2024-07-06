938fa05f67f9acda6269bdfbf51e284e7a3e4146
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-06 05:39:25 +0100

    Fully integrated the tokenomics table

--- components/table/TokenomicsTable/TokenomicsTable.tsx
+++ components/table/TokenomicsTable/TokenomicsTable.tsx
@@ -11,6 +11,16 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 import TokenomicsType from "./cells/TokenomicsType";
 
 const data: TokenomicsColT[] = [
@@ -51,26 +61,88 @@ export const columns: ColumnDef<TokenomicsColT>[] = [
   {
     accessorKey: "type",
     header: "TYPE",
-    cell: ({ row }) => <TokenomicsType type={row.getValue("percentage")} />,
+    cell: ({ row }) => <TokenomicsType type={row.getValue("type")} />,
   },
   {
     accessorKey: "percentage",
     header: "PERCENTAGE",
     cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("percentage")}</div>
+      <div className="capitalize text-neutral-0">
+        {row.getValue("percentage")}%
+      </div>
     ),
   },
   {
     accessorKey: "amount",
     header: "AMOUNT",
     cell: ({ row }) => (
-      <div className="capitalize min-w-[100px]">{row.getValue("amount")}</div>
+      <div className="capitalize text-neutral-0">{row.getValue("amount")}</div>
     ),
   },
 ];
 
 const TokenomicsTable = () => {
-  return <div>TokenomicsTable</div>;
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+    },
+  });
+  return (
+    <Table className="">
+      <TableHeader className="  text-neutral-0">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow
+            key={headerGroup.id}
+            className="rounded-tl-[10px] rounded-tr-[10px]"
+          >
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead key={header.id} className=" text-neutral-0 py-[5px]">
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              );
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody className="bg-[#111111] px-[10px] py-[10px] ">
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow
+              className="border-b-[1px] border-[#13191F] py-[20px]"
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id} className="p-2">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={columns.length} className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default TokenomicsTable;
--- containers/presaleDetails/Tokenomics.tsx
+++ containers/presaleDetails/Tokenomics.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PresalePieChart from "./components/PresalePieChart";
+import TokenomicsTable from "@/components/table/TokenomicsTable/TokenomicsTable";
 
 const Tokenomics = () => {
   return (
@@ -7,8 +8,13 @@ const Tokenomics = () => {
       <h1 className="text-neutral-0 text-[20px] font-[700] mb-[20px]">
         Tokenomics
       </h1>
+      {/* Chart */}
       <div className="w-full h-[400px] flex items-center justify-center">
         <PresalePieChart />
+      </div>
+      {/* Table */}
+      <div>
+        <TokenomicsTable />
       </div>
     </div>
   );
