7a99d02b7ad24f91ecb1dcb62694b4ca58132a96
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-07 10:47:10 +0100

    Added the custom tab one to the trade section

--- containers/tokenDetails/TradeSection.tsx
+++ containers/tokenDetails/TradeSection.tsx
@@ -5,6 +5,7 @@ import PriceItem from "./PriceItem";
 import Image from "next/image";
 import BuySection from "./BuySection";
 import SellSection from "./SellSection";
+import CustomTabOne from "@/components/ui/CustomTab/CustomTabOne";
 
 const TradeSection = () => {
   const [activeTab, setActiveTab] = React.useState("Buy");
@@ -12,33 +13,20 @@ const TradeSection = () => {
     <div className="">
       {/* Buy and sell Tab */}
       <ul className="flex">
-        <button
-          type="button"
+        <CustomTabOne
+          label="Buy"
+          isActive={activeTab === "Buy"}
           onClick={() => {
             setActiveTab("Buy");
           }}
-          className={`text-neutral-0 font-[700] flex items-center justify-center w-[50%] py-[10px] ${
-            activeTab === "Buy"
-              ? "bg-linear-gradient border-one "
-              : " bg-linear-gradient-inactive"
-          } rounded-tl-[5px] rounded-tr-[5px]`}
-        >
-          Buy
-        </button>
-        <button
-          type="button"
+        />
+        <CustomTabOne
+          label="Sell"
+          isActive={activeTab === "Sell"}
           onClick={() => {
             setActiveTab("Sell");
           }}
-          className={`text-neutral-150 font-[700] flex items-center justify-center w-[50%] py-[10px] 
-          rounded-tr-[5px] ${
-            activeTab === "Sell"
-              ? "bg-linear-gradient  border-one"
-              : "bg-linear-gradient-inactive"
-          } `}
-        >
-          Sell
-        </button>
+        />
       </ul>
 
       <div className="border-[1px] border-one rounded-bl-[5px] rounded-br-[5px] py-[20px] px-[10px]">
