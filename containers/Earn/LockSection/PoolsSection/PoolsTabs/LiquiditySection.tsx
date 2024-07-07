e15d63b7d83e1b228c91af8c8e1a8e71d969690c
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-07 11:47:24 +0100

    finished the ui conversion for the liquidity section

--- containers/Earn/LockSection/PoolsSection/PoolsTabs/LiquiditySection.tsx
+++ containers/Earn/LockSection/PoolsSection/PoolsTabs/LiquiditySection.tsx
@@ -6,9 +6,9 @@ import { PiSwapBold } from "react-icons/pi";
 const LiquiditySection = () => {
   return (
     <div className="py-[40px]">
-      <div className="flex justify-center gap-[30px]">
+      <div className="flex  gap-[20px] justify-center items-center">
         {/* Swap Section */}
-        <div className="flex gap-[10px] relative">
+        <div className="flex gap-[10px] relative w-[60%]">
           {/* Approve ETH */}
           <div className="w-[50%]">
             <button className="mb-[10px] text-[13px] bg-custom-radial text-neutral-0 py-[5px] px-[30px] rounded-[4px]">
@@ -23,15 +23,15 @@ const LiquiditySection = () => {
             </ul>
           </div>
 
-          {/* Swap Icon */}
+          {/* Swap toggle */}
           <div className="relative">
-            <button className="top-[50px] left-[-20px] absolute h-[30px] w-[40px] bg-[#2C6397] rounded-[8px] flex justify-center items-center">
+            <button className="top-[55px] left-[-20px] absolute h-[30px] w-[40px] bg-[#2C6397] rounded-[8px] flex justify-center items-center">
               <PiSwapBold className="text-neutral-0" size={15} />
             </button>
           </div>
 
           {/* Approve FTM */}
-          <div className="min-w-[300px]">
+          <div className="w-[50%]">
             <button className="mb-[10px] text-[13px] bg-custom-radial text-neutral-0 py-[5px] px-[30px] rounded-[4px]">
               Approve FTM
             </button>
@@ -40,9 +40,13 @@ const LiquiditySection = () => {
         </div>
 
         {/* Add and remove Lp */}
-        <div>
-          <button>Add Lp</button>
-          <button>Remove Lp</button>
+        <div className="flex flex-col gap-[10px]">
+          <button className="text-[15px] bg-custom-radial text-neutral-0 py-[8px] px-[50px] rounded-[4px]">
+            Add Lp
+          </button>
+          <button className="text-[15px] rounded-[4px] text-neutral-0 border-[2px] border-[#003CFF] py-[8px] w-full text-center">
+            Remove Lp
+          </button>
         </div>
       </div>
     </div>
