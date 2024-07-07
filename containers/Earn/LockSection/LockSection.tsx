96ab8b739da215e969a9f0c247ec5b754d204437
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-07 09:53:46 +0100

    Added the stake and create new pair to the lock section

--- containers/Earn/LockSection/LockSection.tsx
+++ containers/Earn/LockSection/LockSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LockOverView from "./LockOverView";
 import StakeSection from "./StakeSection/StakeSection";
+import PoolsSection from "./PoolsSection/PoolsSection";
 
 const LockSection = () => {
   return (
@@ -14,6 +15,22 @@ const LockSection = () => {
             <LockOverView />
           </div>
         </div>
+
+        {/* Visible on only mobile */}
+        <div className="flex flex-col gap-[10px]">
+          <button className="w-full lg:hidden mt-[30px] bg-custom-radial text-neutral-0 py-[10px] px-[30px] rounded-[5px]">
+            Stake
+          </button>
+          <button
+            type="button"
+            className="w-full lg:hidden text-neutral-0 border-[2px] border-[#47A7FF] bg-[#0D0D0D] rounded-[4px] py-[10px] px-[10px]"
+          >
+            Create New Pair
+          </button>
+        </div>
+
+        {/* Pools */}
+        <PoolsSection />
       </div>
     </div>
   );
