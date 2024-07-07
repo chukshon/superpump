5e5c069c711c205179792d10e3ecf9f41d254072
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-07 10:02:42 +0100

    temporarily created the pool item

---
+++ containers/Earn/LockSection/PoolsSection/PoolsItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PoolsItem = () => {
+  return <div>Pools Item</div>;
+};
+
+export default PoolsItem;
--- containers/Earn/LockSection/PoolsSection/PoolsSection.tsx
+++ containers/Earn/LockSection/PoolsSection/PoolsSection.tsx
@@ -1,12 +1,13 @@
 import SelectVariantOne from "@/components/ui/SelectVariantOne";
 import React from "react";
+import PoolsItem from "./PoolsItem";
 
 const PoolsSection = () => {
   return (
     <div className="mt-[50px]">
       {/*Search Input and filter section */}
-      <div className="flex flex-col justify-between gap-[20px] lg:gap-[50px] lg:flex-row">
-        <div className="w-full flex items-center gap-[10px]">
+      <div className="flex flex-col justify-between gap-[20px] lg:gap-[40px] lg:flex-row">
+        <div className="w-full flex items-center gap-[20px]">
           <input
             placeholder="Search Pools"
             className="z-40 py-[10px] px-[10px] w-full lg:w-[70%] outline-none bg-[transparent] placeholder:text-[16px] text-[16px] placeholder:text-[#4A4A4ACC] text-neutral-50 active:bg-[transparent] placeholder:font-[600] font-space_grotesk border-[1px] rounded-[4px]"
@@ -26,7 +27,9 @@ const PoolsSection = () => {
       </div>
 
       {/* Pools List */}
-      <div>Pools list</div>
+      <ul className="flex flex-col gap-[20px] mt-[20px]">
+        <PoolsItem />
+      </ul>
     </div>
   );
 };
