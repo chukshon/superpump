8d645f99807c7be64d0e6ea133d4e3d32e3b71ec
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-11 21:02:47 +0100

    Restyled the inputTab border

--- containers/Earn/components/InputTab.tsx
+++ containers/Earn/components/InputTab.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const InputTab = ({ value }: { value: string }) => {
   return (
-    <button className="text-[13px] rounded-[4px] text-neutral-0 border-[1px] border-[#112545] bg-[rgba(0,0,0,0.24)] px-[20px] py-[1px]">
+    <button className="text-[13px] rounded-[4px] text-neutral-0 border-[1px] border-[rgba(71,167,255,0.24)] bg-[rgba(0,0,0,0.24)] px-[20px] py-[1px]">
       {value}
     </button>
   );
