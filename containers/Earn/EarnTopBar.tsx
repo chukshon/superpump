9714f9d1c63de7fc052c67c05244aa79d5b1d4b4
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-11 21:55:32 +0100

    Added the active states to the links in the topbar and properly linked it

--- containers/Earn/EarnTopBar.tsx
+++ containers/Earn/EarnTopBar.tsx
@@ -1,16 +1,34 @@
 "use client";
 import LiquidStakingModal from "@/components/Modals/LiquidStakingModal";
 import { useModalContext } from "@/context/ModalContext";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const EarnTopBar = () => {
   const { showModal } = useModalContext();
+  const pathname = usePathname();
   const isActive = false;
+
   return (
-    <div className="border-b-[1px] border-[#828282] py-[10px]">
-      <ul className="text-neutral-0 flex justify-center gap-[30px] font-[700]">
-        <li className={`text-[#47A7FF] text-[15px]`}>Earn</li>
-        <li>The Fomo</li>
+    <div className="text-neutral-0 border-b-[1px] border-[#828282] py-[10px]">
+      <ul className="flex justify-center gap-[30px] font-[700]">
+        <Link
+          href="/earn"
+          className={`${
+            pathname === "/earn" ? "text-[#47A7FF]" : "text-neutral-0"
+          }  text-[15px]`}
+        >
+          Earn
+        </Link>
+        <Link
+          href="earn/fomo"
+          className={`${
+            pathname === "/earn/fomo" ? "text-[#47A7FF]" : "text-neutral-0"
+          }  text-[15px]`}
+        >
+          The Fomo
+        </Link>
         <button
           type="button"
           onClick={() => {
