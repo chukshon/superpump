123b292317dbc6aba6cba8da6f9655633a71c8de
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-11 20:58:28 +0100

    Restructured the durationSelector component

--- containers/Earn/components/DurationSelector.tsx
+++ containers/Earn/components/DurationSelector.tsx
@@ -1,23 +1,38 @@
 import Image from "next/image";
 import React from "react";
+import InputTab from "./InputTab";
 
-const DurationSelector = () => {
+type DurationSelectorProps = {
+  label: string;
+};
+const DurationSelector = ({ label }: DurationSelectorProps) => {
   return (
-    <div className="border-[#172532] border-[1.5px] rounded-[8px] flex justify-between items-center h-[60px] px-[10px]">
-      {/* Left */}
-      <div className="flex items-center gap-[10px] ">
-        <Image
-          src="/icons/clock-logo.svg"
-          width="32"
-          height="32"
-          alt="Eth Logo"
-          className="z-30 cursor-pointer"
-        />
-        <p className="text-neutral-0 text-[18px]">32</p>
-      </div>
+    <div>
+      <h3 className="text-[#47A7FF] font-[600] text-[15px] mb-[10px]">
+        {label}
+      </h3>
+      <div className="border-variant-one bg-[rgba(0,0,0,0.24)] rounded-[8px] flex justify-between items-center h-[60px] px-[10px]">
+        {/* Left */}
+        <div className="flex items-center gap-[10px] ">
+          <Image
+            src="/icons/clock-logo.svg"
+            width="32"
+            height="32"
+            alt="Eth Logo"
+            className="z-30 cursor-pointer"
+          />
+          <p className="text-neutral-0 text-[18px]">32</p>
+        </div>
 
-      {/* Right */}
-      <p className="text-[#4A4A4A] text-[13px]">Weeks(s)</p>
+        {/* Right */}
+        <p className="text-[#4A4A4A] text-[13px]">Weeks(s)</p>
+      </div>
+      <ul className="flex justify-between w-full mt-[20px]">
+        <InputTab value="1W" />
+        <InputTab value="1M" />
+        <InputTab value="3M" />
+        <InputTab value="6M" />
+      </ul>
     </div>
   );
 };
