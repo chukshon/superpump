fc4bfe2584fd872f5750a3e767c7582f42d0fbdc
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-11 21:03:09 +0100

    Added the lock period component

--- components/Modals/CreateNewPairModal/index.tsx
+++ components/Modals/CreateNewPairModal/index.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import Modal from "../Modal";
 import Divider from "@/components/ui/Divider";
 import TokenSelectorVariantII from "@/components/ui/TokenSelectorVariantII/TokenSelectorVariantII";
+import DurationSelector from "@/containers/Earn/components/DurationSelector";
 
 const CreateNewPairModal = () => {
   return (
     <Modal modalHeaderTitle={`New Pair`} width={400}>
       <div className="px-[20px] h-[500px]">
         <Divider className="bg-[rgba(71,167,255,0.24)] h-[1.5px]" />
+
+        {/* First Token  */}
         <div className="mt-[20px]">
           <TokenSelectorVariantII
             label="Select First Token:"
@@ -17,6 +20,8 @@ const CreateNewPairModal = () => {
             tokenImage="/icons/wftm-logo.svg"
           />
         </div>
+
+        {/* Second Token */}
         <div className="mt-[20px]">
           <TokenSelectorVariantII
             label="Select Second Token:"
@@ -24,6 +29,11 @@ const CreateNewPairModal = () => {
             tokenTicker={"WETH"}
             tokenImage="/icons/eth-logo.svg"
           />
+        </div>
+
+        {/* Lock Period */}
+        <div className="mt-[20px]">
+          <DurationSelector label=" Lock Period" />
         </div>
       </div>
     </Modal>
