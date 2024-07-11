42b147a99e0aad5cdc42c68f8b42316cf4c2b858
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-11 13:37:51 +0100

    Added a min height to the text area component

--- components/FormFields/TextArea.tsx
+++ components/FormFields/TextArea.tsx
@@ -10,7 +10,7 @@ const TextArea = ({ name, placeholder, className }: TextAreaProps) => {
     <textarea
       name={name}
       placeholder={placeholder}
-      className={`${className} rounded-[8px] text-neutral-0 pl-[10px] w-full bg-[rgba(0,0,0,0.24)] py-[8px] border-variant-one placeholder:text-[rgba(130,130,130,0.56)] bg-transparent outline-none`}
+      className={`${className} min-h-[100px] rounded-[8px] text-neutral-0 pl-[10px] w-full bg-[rgba(0,0,0,0.24)] py-[8px] border-variant-one placeholder:text-[rgba(130,130,130,0.56)] bg-transparent outline-none`}
     />
   );
 };
