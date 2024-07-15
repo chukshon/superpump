069fe21a42b3fc6c618a4d2b2efd4446032e9673
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-07-15 09:28:58 +0100

    restyled the desktop nav dropdown

--- components/ui/DropdownVariantOne.tsx
+++ components/ui/DropdownVariantOne.tsx
@@ -23,15 +23,18 @@ const DropdownVariantOne = ({
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="text-neutral-0 flex items-center gap-[5px] outline-none">
-        <p>{selectedItem}</p>
+      <DropdownMenuTrigger className="justify-between rounded-sm px-2 py-1.5 bg-[#3C3C3C] min-w-[200px] text-[#828282] flex items-center gap-[5px] outline-none">
+        <p className="text-[#828282]">{selectedItem}</p>
         <FaAngleDown />
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent
+        align="start"
+        className="bg-[#3C3C3C] border-none text-neutral-0 min-w-[200px] mt-[5px]"
+      >
         {dropDownMenuContent.map((item) => {
           return (
             <DropdownMenuItem
-              className="cursor-pointer capitalize"
+              className="border-t-[2px] border-[#444444] bg-[#3C3C3C] rounded-none cursor-pointer capitalize"
               onClick={() => {
                 setSelectedItem(item.label);
                 item.onClick();
