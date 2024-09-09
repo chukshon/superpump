723520dd267704bdc4d8c96ee1cd7f0b06ce3800
ChukwukaHonour <ChukwukaHonour@gmail.com>
2024-09-09 11:55:54 +0100

    consume the buy token contract

--- containers/tokenDetails/BuySection.tsx
+++ containers/tokenDetails/BuySection.tsx
@@ -1,13 +1,89 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PriceItem from "./PriceItem";
 import Image from "next/image";
 import { RiArrowDownSLine } from "react-icons/ri";
 import { useModalContext } from "@/context/ModalContext";
 import SelectTokenModal from "@/components/Modals/SelectTokenModal.tsx";
-import { Select } from "@radix-ui/react-select";
+import { BaseError } from "viem";
+import { useWriteContract, useReadContract } from "wagmi";
+import {
+  factoryContractConfig,
+  poolExecutionerContractConfig,
+} from "@/contracts/contracts";
+import { toBytes } from "viem";
+import { parseEther, formatEther, parseGwei } from "viem";
 
 const BuySection = () => {
   const { showModal } = useModalContext();
+
+  const { writeContract, isPending, error } = useWriteContract();
+
+  const handleBuy = () => {
+    writeContract({
+      ...poolExecutionerContractConfig,
+      address: "0xBCeE2669070dAF7Fb53f8071Db1E0c111a34276f",
+      functionName: "buyToken_Qdy",
+      args: [parseEther("0.00000001")],
+      value: parseEther("0.00000001"),
+    });
+  };
+
+  const {
+    data: showPoolAddress,
+    isLoading: getGreetingLoading,
+    isError: getGreetingError,
+    refetch: refetchGreeting,
+  } = useReadContract({
+    ...factoryContractConfig,
+    functionName: "showPoolAddress",
+    args: ["0xe9C34aBe02B1cfc9388C56f4eE9c32263744caBb"],
+  });
+
+  const {
+    data: USDPerToken,
+    isLoading: USDPerTokenLoading,
+    isError: USDPerTokenError,
+    refetch: refetchUSDPerToken,
+  } = useReadContract({
+    ...poolExecutionerContractConfig,
+    address: "0xBCeE2669070dAF7Fb53f8071Db1E0c111a34276f",
+    functionName: "USDPerToken",
+  });
+
+  useEffect(() => {
+    if (showPoolAddress) {
+      console.log(
+        showPoolAddress,
+        // formatEther(showPoolAddress as bigint, "wei"),
+        "showPoolAddress"
+      );
+    }
+  }, [showPoolAddress]);
+
+  useEffect(() => {
+    if (USDPerToken) {
+      console.log(
+        formatEther(USDPerToken as bigint, "wei"),
+        // formatEther(showPoolAddress as bigint, "wei"),
+        "USDPerToken"
+      );
+    }
+  }, [USDPerToken]);
+
+  // useEffect(() => {
+  //   if (nativeBase) {
+  //     console.log(nativeBase, "nativeBase");
+  //   }
+  // }, [nativeBase]);
+
+  useEffect(() => {
+    console.log(isPending, "isPending");
+  }, [isPending]);
+
+  useEffect(() => {
+    console.log((error as BaseError)?.shortMessage || error?.message, "error");
+  }, [error]);
+
   return (
     <div>
       {/* Fixed Price List */}
@@ -78,6 +154,7 @@ const BuySection = () => {
       {/* Quick Buy/Sell */}
       <button
         type="button"
+        onClick={() => handleBuy()}
         className="text-neutral-0 rounded-[4px] font-[600] text-[16px] bg-green-radial-gradient w-full text-center py-[10px]"
       >
         Quick Buy
